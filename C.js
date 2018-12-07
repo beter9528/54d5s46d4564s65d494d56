cdc7cf1a93e06a4cb996c48fecece7d5e8a46447
beter9528 <45292695+beter9528@users.noreply.github.com>
2018-12-07 13:52:36 +0200

    Add files via upload

---
+++ C.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+
+
+client.login(''); 
+
+
+
+
+client.on('message', message => {
+     if (message.content === "") {
+message.author.send("**G=help**" + `  **
+شكرأ لاستعمالك البوت**
+`1-`**Soon**
+`2-`**Soon**
+`3-`**Soon**
+`3-`**Soon**
+ `3-`**Soon**
+
+**`);
+    }
+})
